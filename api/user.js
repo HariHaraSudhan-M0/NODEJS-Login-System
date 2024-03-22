ad0e8271ef3975e652c3a3a214656c3baed92206
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-03-22 10:19:13 +0530

    allowing users to take inputs from given requests

--- api/user.js
+++ api/user.js
@@ -2,7 +2,8 @@ const express=require('express');
 const router=express.Router()
 const app =require('express')();
 router.post('/signup',(req,res)=>{
-
+    let{name,email,password,dateOfBirth}=req.body;
+    name
 })
 router.post('/signin',(req,res)=>{
     
